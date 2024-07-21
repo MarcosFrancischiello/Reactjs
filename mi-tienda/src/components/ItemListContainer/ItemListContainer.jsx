ffbb3e7fe9c49bd4bcfbdb2fc88ae349847d8b59
Marcos Francischiello <marcosfrancischiello1@gmail.com>
2024-07-21 18:54:03 -0300

    Primera entrega

---
+++ mi-tienda/src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './ItemListContainer.css'
+const ItemListContainer = ({saludo}) => {
+    return (
+    <div className='saludo'>{saludo}</div>
+)
+}
+
+export default ItemListContainer
---
+++ mi-tienda/src/components/Navbar/CartWidget/CartWidget.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CartWidget.css'
+const CartWidget = () => {
+    return (
+    <div className='CartWidget'>
+        🛒
+        <span>0</span>
+        </div>
+)
+}
+
+export default CartWidget
